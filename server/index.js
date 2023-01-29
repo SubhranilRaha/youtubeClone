75490e78f0287f7170be665adb897a5875201b7b
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2023-01-30 01:17:20 +0530

    rest api added to server

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import userRoutes from "./routes/users.js"
+import videoRoutes from "./routes/videos.js"
+import commentRoutes from "./routes/comments.js"
+
+const app=express()
+dotenv.config()
+
+const connect = () =>{
+    mongoose.connect(process.env.MONGO,{
+    }).then(()=>{
+        console.log("Connected to DB!")
+    }).catch((err)=>{
+        throw err;
+    })
+}
+
+mongoose.set('strictQuery', false);
+
+app.use("/",userRoutes)
+app.use("/",videoRoutes)
+app.use("/",commentRoutes)
+
+app.listen(8800,()=>{
+    connect()
+    console.log("Connected to server!")
+})
