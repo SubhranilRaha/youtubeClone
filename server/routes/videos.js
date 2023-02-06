388f1aaacc0661d8783cfe7f6c0e31bbacd054db
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2023-02-06 21:24:05 +0530

    video crud

--- server/routes/videos.js
+++ server/routes/videos.js
@@ -1,6 +1,18 @@
 import express from "express";
-import {  } from "../controllers/videos.js";
+import { addVideo, addView, getByTag, getVideo, random, search, sub, trend } from "../controllers/videos.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router=express.Router();
+
+router.post("/", verifyToken, addVideo)
+router.put("/:id", verifyToken, addVideo)
+router.delete("/:id", verifyToken, addVideo)
+router.get("/find/:id", getVideo)
+router.put("/view/:id", addView)
+router.get("/trend", trend)
+router.get("/random", random)
+router.get("/sub",verifyToken, sub)
+router.get("/tags", getByTag)
+router.get("/search", search)
 
 export default router;
